93eb0db783ca3dd2aa842cc1e25bf2729ea85e20
RRayo <rayo.in@gmail.com>
2019-06-26 15:55:54 -0400

    se modifica para agregar directamente dataset y deteccion}2

--- label_processing.py
+++ label_processing.py
@@ -1,13 +1,25 @@
 import csv
 from pathlib import Path
 import shutil
+import sys
+
+detecciones = 'images_labeled.csv'
+data = "/home/rrayo/train2014"
+out_dir = "."
+
+try:
+    detecciones = sys.argv[1]
+    data = sys.argv[2]
+    out_dir = sys.argv[3]
+except:
+    pass
 
 # funcion para copiar las imagenes con objetos detectados en carpetas segun su clasificación en clusters
 label_dict = dict()
 
 # obtener un diccionario a partir del archivo con los 
 # resultados de la clusterización sobre los objetos detectados en las imagenes
-with open('images_labeled.csv') as csv_file:
+with open(detecciones) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -15,14 +27,14 @@ with open('images_labeled.csv') as csv_file:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            if row[2] not in label_dict:
-                label_dict[row[2]] = []
-            label_dict[row[2]].append(row[1])
+            if row[1] not in label_dict:
+                label_dict[row[1]] = []
+            label_dict[row[1]].append(row[0])
             line_count += 1
     print(f'Processed {line_count} lines.')
 
 # path a la carpeta con las imagenes donde se detecto objetos
-coco_path = Path("/home/rrayo/train2014")
+coco_path = Path(data)
 path = Path(".")
 
 # copia las imagenes en la carpeta correspondiente a la clase asignada
